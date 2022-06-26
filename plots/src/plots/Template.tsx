c825a5c4c1fd48de5f5825fd3ee54f5e4297b5e0
Will Schaller <willschaller@gmail.com>
2022-06-26 13:37:56 +0100

    Add template

---
+++ plots/src/plots/Template.tsx
@@ -0,0 +1,40 @@
+import {
+  Easel,
+  getSizesInPixels,
+  PAPER_SIZES_MILLIMETERS,
+  SvgPlotArea,
+} from '@react-plots/react-plotter-kit';
+import { SvgGroup } from '@react-plots/react-plotter-kit/src/components/SvgGroup';
+import usePlot from '@react-plots/react-plotter-kit/src/lib/usePlot';
+import React from 'react';
+
+const { drawableRegionInPixels, marginInPixels } = getSizesInPixels(
+  PAPER_SIZES_MILLIMETERS.a3.vertical,
+  0.5,
+  {
+    horizontal: 20,
+    vertical: 20,
+  }
+);
+
+export default () => {
+  const { svgGroupProps, svgPlotAreaProps } = usePlot(
+    drawableRegionInPixels,
+    marginInPixels
+  );
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Easel>
+        <SvgPlotArea {...svgPlotAreaProps}>
+          <SvgGroup {...svgGroupProps} centerContents={true}></SvgGroup>
+        </SvgPlotArea>
+      </Easel>
+    </div>
+  );
+};
