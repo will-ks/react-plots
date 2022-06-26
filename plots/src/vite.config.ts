1d2cd6040b79081dc7d80fa425bea3e8ba47ea32
Will Schaller <willschaller@gmail.com>
2022-06-26 21:22:00 +0100

    Tweak config

--- plots/src/vite.config.ts
+++ plots/src/vite.config.ts
@@ -1,17 +1,22 @@
-import type { UserConfig } from 'vite'
-import path from 'path'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import mdx from 'vite-plugin-mdx'
-import pages, {DefaultPageStrategy, extractStaticData, File, FileHandler, PageStrategy} from 'vite-plugin-react-pages'
-import {getPagePublicPath} from "vite-plugin-react-pages/dist/node/page-strategy/DefaultPageStrategy";
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path';
+import type { UserConfig } from 'vite';
+import mdx from 'vite-plugin-mdx';
+import pages, {
+  extractStaticData,
+  File,
+  FileHandler,
+  PageStrategy,
+} from 'vite-plugin-react-pages';
+import { getPagePublicPath } from 'vite-plugin-react-pages/dist/node/page-strategy/DefaultPageStrategy';
 
 const fileHandler: FileHandler = async (file: File, fileHandlerAPI) => {
-    fileHandlerAPI.addPageData({
-        pageId: getPagePublicPath(file.relative).split('.')[0],
-        dataPath: file.path,
-        staticData: await extractStaticData(file),
-    })
-}
+  fileHandlerAPI.addPageData({
+    pageId: getPagePublicPath(file.relative).split('.')[0],
+    dataPath: file.path,
+    staticData: await extractStaticData(file),
+  });
+};
 
 module.exports = {
   jsx: 'react',
@@ -19,20 +24,15 @@ module.exports = {
     reactRefresh(),
     mdx(),
     pages({
-        pagesDir: __dirname,
-        // custom pageStrategy
-        pageStrategy: new PageStrategy(function findPages(pagesDir, helpers) {
-            helpers.watchFiles(
-                path.join(pagesDir, 'plots'),
-                '**/**/*.{md,mdx,js,jsx,ts,tsx}',
-                fileHandler
-            )
-        }),
+      pagesDir: __dirname,
+      // custom pageStrategy
+      pageStrategy: new PageStrategy(function findPages(pagesDir, helpers) {
+        helpers.watchFiles(
+          path.join(pagesDir, 'plots'),
+          '**/**/*.{md,mdx,js,jsx,ts,tsx}',
+          fileHandler
+        );
+      }),
     }),
   ],
-  resolve: {
-    alias: {
-      'my-lib': path.join(__dirname, '../src'),
-    },
-  },
-} as UserConfig
+} as UserConfig;
